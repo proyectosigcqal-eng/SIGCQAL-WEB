9fb44b0ea3dc896a33e2a78473c5e224bc033811
JuanMerino15 <juanrivmerino4@gmail.com>
2026-04-28 15:47:49 -0600

    Feature/Bitacora

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { MainLayout } from './shared/MainLayout'; 
 import { GenerarMemorandumPage } from './pages/modulo-correspondencia/memorandum/GenerarMemorandumPage';
-
+import { AsignarAreaPage } from './pages/modulo-correspondencia/memorandum/AsignarAreaPage'; 
 import { BitacoraHistorica } from './features/modulo-correspondencia/bitacora-historica/components/BitacoraHistorica';
 
 function App() {
@@ -10,7 +10,14 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Navigate to="correspondencia/nuevo-memorandum" replace />} />
+          
+          {/* Flujo de creación de Memorándum */}
           <Route path="correspondencia/nuevo-memorandum" element={<GenerarMemorandumPage />} />
+          
+          {/* 2. Nueva ruta para Asignar Área (Paso posterior a la generación) */}
+          <Route path="correspondencia/asignar-area/:id" element={<AsignarAreaPage />} />
+
+          {/* Consulta de Bitácora */}
           <Route path="correspondencia/bitacora/:id" element={<BitacoraHistorica />} />
 
           {/* <Route path="correspondencia" element={<CorrespondenciaPage />} /> */}
