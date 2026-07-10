e3312ee05fea0665a261073a228223184d9f09e4
JuanMerino15 <juanrivmerino4@gmail.com>
2026-07-09 23:21:12 -0600

    Corrección en acuse

--- src/features/modulo-correspondencia/correspondencia/services/correspondenciaService.js
+++ src/features/modulo-correspondencia/correspondencia/services/correspondenciaService.js
@@ -22,6 +22,7 @@ export const obtenerCorrespondenciaPorId = async (id) => {
   return response.data;
 };
 
+// correspondenciaService.js
 export const obtenerCorrespondenciaPendientePorArea = async (idArea) => {
   const response = await axios.get(`${API_URL}/pendienteacuse/area/${idArea}`);
   return response.data;
